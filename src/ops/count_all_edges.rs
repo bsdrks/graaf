da36ea99cdbebd65623175828f0da145a02e1ce6
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-18 18:47:46 +0100

    feature: implement `CountAllEdges` for `HashMap`

--- src/ops/count_all_edges.rs
+++ src/ops/count_all_edges.rs
@@ -10,6 +10,8 @@ pub trait CountAllEdges {
     fn count_all_edges(&self) -> usize;
 }
 
+// Vec
+
 impl<T> CountAllEdges for Vec<Vec<T>> {
     /// # Panics
     ///
@@ -23,36 +25,7 @@ impl<T> CountAllEdges for Vec<Vec<T>> {
     }
 }
 
-impl<const V: usize, T> CountAllEdges for [Vec<T>; V] {
-    /// # Panics
-    ///
-    /// Panics if the number of edges exceeds `usize::MAX`.
-    ///
-    /// # Complexity
-    ///
-    /// O(V)
-    fn count_all_edges(&self) -> usize {
-        self.iter().map(Vec::len).sum()
-    }
-}
-
 impl<T, H> CountAllEdges for Vec<HashSet<T, H>>
-where
-    H: BuildHasher,
-{
-    /// # Panics
-    ///
-    /// Panics if the number of edges exceeds `usize::MAX`.
-    ///
-    /// # Complexity
-    ///
-    /// O(V)
-    fn count_all_edges(&self) -> usize {
-        self.iter().map(HashSet::len).sum()
-    }
-}
-
-impl<const V: usize, T, H> CountAllEdges for [HashSet<T, H>; V]
 where
     H: BuildHasher,
 {
@@ -84,6 +57,37 @@ where
     }
 }
 
+// Array
+
+impl<const V: usize, T> CountAllEdges for [Vec<T>; V] {
+    /// # Panics
+    ///
+    /// Panics if the number of edges exceeds `usize::MAX`.
+    ///
+    /// # Complexity
+    ///
+    /// O(V)
+    fn count_all_edges(&self) -> usize {
+        self.iter().map(Vec::len).sum()
+    }
+}
+
+impl<const V: usize, T, H> CountAllEdges for [HashSet<T, H>; V]
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if the number of edges exceeds `usize::MAX`.
+    ///
+    /// # Complexity
+    ///
+    /// O(V)
+    fn count_all_edges(&self) -> usize {
+        self.iter().map(HashSet::len).sum()
+    }
+}
+
 impl<const V: usize, W, H> CountAllEdges for [HashMap<usize, W, H>; V]
 where
     H: BuildHasher,
@@ -97,5 +101,55 @@ where
     /// O(V)
     fn count_all_edges(&self) -> usize {
         self.iter().map(HashMap::len).sum()
+    }
+}
+
+// HashMap
+
+impl<K, V, H> CountAllEdges for HashMap<K, Vec<V>, H>
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if the number of edges exceeds `usize::MAX`.
+    ///
+    /// # Complexity
+    ///
+    /// O(V)
+    fn count_all_edges(&self) -> usize {
+        self.values().map(Vec::len).sum()
+    }
+}
+
+impl<K, H> CountAllEdges for HashMap<K, HashSet<usize, H>, H>
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if the number of edges exceeds `usize::MAX`.
+    ///
+    /// # Complexity
+    ///
+    /// O(V)
+    fn count_all_edges(&self) -> usize {
+        self.values().map(HashSet::len).sum()
+    }
+}
+
+impl<K, W, H> CountAllEdges for HashMap<K, HashMap<usize, W, H>, H>
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if the number of edges exceeds `usize::MAX`.
+    ///
+    /// # Complexity
+    ///
+    /// O(V)
+    fn count_all_edges(&self) -> usize {
+        self.values().map(HashMap::len).sum()
     }
 }
