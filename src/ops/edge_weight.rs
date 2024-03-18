8b8200f43c3709587dcbff1f8f6254af090aaacc
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-18 18:56:25 +0100

    feature: implement `EdgeWeight` for `HashMap`

--- src/ops/edge_weight.rs
+++ src/ops/edge_weight.rs
@@ -7,6 +7,8 @@ pub trait EdgeWeight<W> {
     fn edge_weight(&self, s: usize, t: usize) -> W;
 }
 
+// Vec
+
 impl<W, H> EdgeWeight<W> for Vec<HashMap<usize, W, H>>
 where
     H: BuildHasher,
@@ -25,6 +27,8 @@ where
     }
 }
 
+// Arr
+
 impl<const V: usize, W, H> EdgeWeight<W> for [HashMap<usize, W, H>; V]
 where
     H: BuildHasher,
@@ -40,5 +44,24 @@ where
     /// O(1)
     fn edge_weight(&self, s: usize, t: usize) -> W {
         self[s][&t]
+    }
+}
+
+// HashMap
+
+impl<W, H> EdgeWeight<W> for HashMap<usize, HashMap<usize, W, H>>
+where
+    H: BuildHasher,
+    W: Copy,
+{
+    /// # Panics
+    ///
+    /// Panics if there is no edge from `s` to `t`.
+    ///
+    /// # Complexity
+    ///
+    /// O(1)
+    fn edge_weight(&self, s: usize, t: usize) -> W {
+        self[&s][&t]
     }
 }
