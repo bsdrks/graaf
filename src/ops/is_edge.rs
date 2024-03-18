c5545c3636d446d0b07f84a3342a354a67768927
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-18 18:59:57 +0100

    feature: implement `IsEdge` for `HashMap`

--- src/ops/is_edge.rs
+++ src/ops/is_edge.rs
@@ -10,23 +10,9 @@ pub trait IsEdge {
     fn is_edge(&self, s: usize, t: usize) -> bool;
 }
 
-impl<H> IsEdge for Vec<HashSet<usize, H>>
-where
-    H: BuildHasher,
-{
-    /// # Panics
-    ///
-    /// Panics if `s` is out of bounds.
-    ///
-    /// # Complexity
-    ///
-    /// TODO
-    fn is_edge(&self, s: usize, t: usize) -> bool {
-        self[s].contains(&t)
-    }
-}
+// Vec
 
-impl<const V: usize, H> IsEdge for [HashSet<usize, H>; V]
+impl<H> IsEdge for Vec<HashSet<usize, H>>
 where
     H: BuildHasher,
 {
@@ -58,6 +44,24 @@ where
     }
 }
 
+// Arr
+
+impl<const V: usize, H> IsEdge for [HashSet<usize, H>; V]
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if `s` is out of bounds.
+    ///
+    /// # Complexity
+    ///
+    /// TODO
+    fn is_edge(&self, s: usize, t: usize) -> bool {
+        self[s].contains(&t)
+    }
+}
+
 impl<const V: usize, W, H> IsEdge for [HashMap<usize, W, H>; V]
 where
     H: BuildHasher,
@@ -71,5 +75,39 @@ where
     /// TODO: O(1)?
     fn is_edge(&self, s: usize, t: usize) -> bool {
         self[s].contains_key(&t)
+    }
+}
+
+// HashMap
+
+impl<H> IsEdge for HashMap<usize, HashSet<usize, H>>
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if `s` is out of bounds.
+    ///
+    /// # Complexity
+    ///
+    /// TODO
+    fn is_edge(&self, s: usize, t: usize) -> bool {
+        self[&s].contains(&t)
+    }
+}
+
+impl<W, H> IsEdge for HashMap<usize, HashMap<usize, W, H>>
+where
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if `s` is out of bounds.
+    ///
+    /// # Complexity
+    ///
+    /// TODO
+    fn is_edge(&self, s: usize, t: usize) -> bool {
+        self[&s].contains_key(&t)
     }
 }
