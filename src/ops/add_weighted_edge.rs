c028acaf4188264cbd74510e31d08f3cea3b84db
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-18 18:37:52 +0100

    feature: implement `AddWeightedEdge` for `HashMap` variants

--- src/ops/add_weighted_edge.rs
+++ src/ops/add_weighted_edge.rs
@@ -105,6 +105,32 @@ where
     }
 }
 
+impl<W, H> AddWeightedEdge<W> for HashMap<usize, Vec<(usize, W)>, H>
+where
+    H: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(1)
+    fn add_weighted_edge(&mut self, s: usize, t: usize, w: W) {
+        self.entry(s).or_default().push((t, w));
+    }
+}
+
+impl<W, H> AddWeightedEdge<W> for HashMap<usize, HashSet<(usize, W), H>, H>
+where
+    H: BuildHasher,
+    W: Eq + Hash,
+    HashSet<(usize, W), H>: Default,
+{
+    /// # Complexity
+    ///
+    /// O(1)
+    fn add_weighted_edge(&mut self, s: usize, t: usize, w: W) {
+        self.entry(s).or_default().insert((t, w));
+    }
+}
+
 impl<W, H> AddWeightedEdge<W> for HashMap<usize, HashMap<usize, W, H>, H>
 where
     H: BuildHasher,
