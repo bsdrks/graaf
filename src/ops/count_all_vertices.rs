83fb5a51889e459b93a3d3bc0a241d933d7fe229
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-18 18:49:07 +0100

    feature: implement `CountAllVertices` for `HashMap`

--- src/ops/count_all_vertices.rs
+++ src/ops/count_all_vertices.rs
@@ -1,6 +1,16 @@
+use std::{
+    collections::HashMap,
+    hash::{
+        BuildHasher,
+        Hash,
+    },
+};
+
 pub trait CountAllVertices {
     fn count_all_vertices(&self) -> usize;
 }
+
+// Vec
 
 impl<T> CountAllVertices for Vec<T> {
     /// # Complexity
@@ -11,11 +21,28 @@ impl<T> CountAllVertices for Vec<T> {
     }
 }
 
+// Array
+
 impl<const V: usize, T> CountAllVertices for [T; V] {
     /// # Complexity
     ///
     /// O(1)
     fn count_all_vertices(&self) -> usize {
         V
+    }
+}
+
+// HashMap
+
+impl<K, V, S> CountAllVertices for HashMap<K, V, S>
+where
+    K: Hash + Eq,
+    S: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(1)
+    fn count_all_vertices(&self) -> usize {
+        self.len()
     }
 }
