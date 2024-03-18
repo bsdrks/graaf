819e7d85b81dcef0e9be62ebd781e5001b7f3c89
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-18 19:06:19 +0100

    feature: implement `IterAllEdges` for `HashMap`

--- src/ops/iter_all_edges.rs
+++ src/ops/iter_all_edges.rs
@@ -1,6 +1,16 @@
+use std::{
+    collections::{
+        HashMap,
+        HashSet,
+    },
+    hash::BuildHasher,
+};
+
 pub trait IterAllEdges {
     fn iter_all_edges(&self) -> impl Iterator<Item = (usize, usize)>;
 }
+
+// Vec
 
 impl IterAllEdges for Vec<(usize, usize)> {
     /// # Complexity
@@ -11,11 +21,41 @@ impl IterAllEdges for Vec<(usize, usize)> {
     }
 }
 
+// Arr
+
 impl<const V: usize> IterAllEdges for [(usize, usize); V] {
     /// # Complexity
     ///
     /// O(V)
     fn iter_all_edges(&self) -> impl Iterator<Item = (usize, usize)> {
         self.iter().copied()
+    }
+}
+
+// HashMap
+
+impl<H> IterAllEdges for HashMap<usize, Vec<usize>, H>
+where
+    H: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(V)
+    fn iter_all_edges(&self) -> impl Iterator<Item = (usize, usize)> {
+        self.iter()
+            .flat_map(|(s, t)| t.iter().copied().map(move |t| (*s, t)))
+    }
+}
+
+impl<H> IterAllEdges for HashMap<usize, HashSet<usize, H>>
+where
+    H: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(V)
+    fn iter_all_edges(&self) -> impl Iterator<Item = (usize, usize)> {
+        self.iter()
+            .flat_map(|(s, ts)| ts.iter().map(move |t| (*s, *t)))
     }
 }
