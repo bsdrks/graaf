e60cab671530b153a37805e2ced2b15dc8a2fb8c
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-19 21:51:01 +0100

    feature: implement `IterAllWeightedEdges` for `Vec`, array, and `HashMap`

--- src/ops/iter_all_weighted_edges.rs
+++ src/ops/iter_all_weighted_edges.rs
@@ -1,6 +1,16 @@
+use std::{
+    collections::{
+        HashMap,
+        HashSet,
+    },
+    hash::BuildHasher,
+};
+
 pub trait IterAllWeightedEdges<W> {
     fn iter_all_weighted_edges(&self) -> impl Iterator<Item = (usize, usize, W)>;
 }
+
+// Vec
 
 impl<W> IterAllWeightedEdges<W> for Vec<(usize, usize, W)>
 where
@@ -14,6 +24,8 @@ where
     }
 }
 
+// Arr
+
 impl<const V: usize, W> IterAllWeightedEdges<W> for [(usize, usize, W); V]
 where
     W: Copy,
@@ -23,5 +35,49 @@ where
     /// O(V)
     fn iter_all_weighted_edges(&self) -> impl Iterator<Item = (usize, usize, W)> {
         self.iter().copied()
+    }
+}
+
+// HashMap
+
+impl<W, H> IterAllWeightedEdges<W> for HashMap<usize, Vec<(usize, W)>, H>
+where
+    W: Copy,
+    H: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(V)
+    fn iter_all_weighted_edges(&self) -> impl Iterator<Item = (usize, usize, W)> {
+        self.iter()
+            .flat_map(|(s, ts)| ts.iter().copied().map(move |(t, w)| (*s, t, w)))
+    }
+}
+
+impl<W, H> IterAllWeightedEdges<W> for HashMap<usize, HashSet<(usize, W)>, H>
+where
+    W: Copy,
+    H: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(V)
+    fn iter_all_weighted_edges(&self) -> impl Iterator<Item = (usize, usize, W)> {
+        self.iter()
+            .flat_map(|(s, ts)| ts.iter().copied().map(move |(t, w)| (*s, t, w)))
+    }
+}
+
+impl<W, H> IterAllWeightedEdges<W> for HashMap<usize, HashMap<usize, W, H>>
+where
+    W: Copy,
+    H: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(V)
+    fn iter_all_weighted_edges(&self) -> impl Iterator<Item = (usize, usize, W)> {
+        self.iter()
+            .flat_map(|(s, ts)| ts.iter().map(move |(t, w)| (*s, *t, *w)))
     }
 }
