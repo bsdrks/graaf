22c7365bce9f61e1dda24bef18036dc1a8a67e85
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-19 21:56:42 +0100

    feature: implement `IterVertices` for `HashMap`

--- src/ops/iter_vertices.rs
+++ src/ops/iter_vertices.rs
@@ -1,6 +1,16 @@
+use std::{
+    collections::HashMap,
+    hash::{
+        BuildHasher,
+        Hash,
+    },
+};
+
 pub trait IterVertices {
     fn iter_vertices(&self) -> impl Iterator<Item = usize>;
 }
+
+// Vec
 
 impl<T> IterVertices for Vec<T> {
     /// # Complexity
@@ -11,11 +21,27 @@ impl<T> IterVertices for Vec<T> {
     }
 }
 
+// Arr
+
 impl<T, const V: usize> IterVertices for [T; V] {
     /// # Complexity
     ///
     /// O(V)
     fn iter_vertices(&self) -> impl Iterator<Item = usize> {
         0..V
+    }
+}
+
+// HashMap
+
+impl<V, S> IterVertices for HashMap<usize, V, S>
+where
+    S: BuildHasher,
+{
+    /// # Complexity
+    ///
+    /// O(V)
+    fn iter_vertices(&self) -> impl Iterator<Item = usize> {
+        self.keys().copied()
     }
 }
