c06140b4eb577698b9ae91e7496be36aba44bc7d
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-19 21:49:30 +0100

    bench: finish benching `AddEdge`

--- benches/add_edge.rs
+++ benches/add_edge.rs
@@ -1,26 +1,88 @@
 use {
     graph::AddEdge,
-    std::collections::HashSet,
+    std::{
+        array::from_fn,
+        collections::{
+            HashMap,
+            HashSet,
+        },
+    },
 };
 
 fn main() {
     divan::main();
 }
 
-#[divan::bench(args = [1, 10, 100, 1000, 10_000, 100_000])]
-fn vec(v: usize) {
+const ARGS: [usize; 4] = [1, 10, 100, 1000];
+
+// Vec
+
+#[divan::bench(args = ARGS)]
+fn vec_vec(v: usize) {
     let mut adj = vec![Vec::<usize>::new(); v];
 
-    for i in 0..v {
-        adj.add_edge(i, (i + 1) % v);
+    for s in 0..v {
+        for t in 0..v {
+            adj.add_edge(s, t);
+        }
     }
 }
 
-#[divan::bench(args = [1, 10, 100, 1000, 10_000, 100_000])]
-fn hash_set(v: usize) {
+#[divan::bench(args = ARGS)]
+fn vec_hash_set(v: usize) {
     let mut adj = vec![HashSet::<usize>::new(); v];
 
-    for i in 0..v {
-        adj.add_edge(i, (i + 1) % v);
+    for s in 0..v {
+        for t in 0..v {
+            adj.add_edge(s, t);
+        }
+    }
+}
+
+// Arr
+
+#[divan::bench(consts = ARGS)]
+fn arr_vec<const V: usize>() {
+    let mut adj = from_fn::<Vec<usize>, V, _>(|_| Vec::new());
+
+    for s in 0..V {
+        for t in 0..V {
+            adj.add_edge(s, t);
+        }
+    }
+}
+
+#[divan::bench(consts = ARGS)]
+fn arr_hash_set<const V: usize>() {
+    let mut adj = from_fn::<HashSet<usize>, V, _>(|_| HashSet::new());
+
+    for s in 0..V {
+        for t in 0..V {
+            adj.add_edge(s, t);
+        }
+    }
+}
+
+// HashMap
+
+#[divan::bench(consts = ARGS)]
+fn hash_map_vec<const V: usize>() {
+    let mut adj = HashMap::<usize, Vec<usize>>::new();
+
+    for s in 0..V {
+        for t in 0..V {
+            adj.add_edge(s, t);
+        }
+    }
+}
+
+#[divan::bench(consts = ARGS)]
+fn hash_map_hash_set<const V: usize>() {
+    let mut adj = HashMap::<usize, HashSet<usize>>::new();
+
+    for s in 0..V {
+        for t in 0..V {
+            adj.add_edge(s, t);
+        }
     }
 }
