f0a15b696c14e06555c2834344b7c62e5a1b9987
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-03-20 07:38:15 +0100

    feature: implement `IterWeightedEdges` for `T<HashSet>`

--- src/ops/iter_weighted_edges.rs
+++ src/ops/iter_weighted_edges.rs
@@ -2,6 +2,8 @@ pub trait IterWeightedEdges<W> {
     fn iter_weighted_edges(&self, s: usize) -> impl Iterator<Item = (usize, W)>;
 }
 
+// Vec
+
 impl<W> IterWeightedEdges<W> for Vec<Vec<(usize, W)>>
 where
     W: Copy,
@@ -18,7 +20,42 @@ where
     }
 }
 
+impl<W, H> IterWeightedEdges<W> for Vec<HashSet<(usize, W), H>>
+where
+    W: Copy,
+    H: BuildHasher,
+{
+    /// # Panics
+    ///
+    /// Panics if `s` is out of bounds.
+    ///
+    /// # Complexity
+    ///
+    /// O(1)
+    fn iter_weighted_edges(&self, s: usize) -> impl Iterator<Item = (usize, W)> {
+        self[s].iter().copied()
+    }
+}
+
+// Arr
+
 impl<const V: usize, W> IterWeightedEdges<W> for [Vec<(usize, W)>; V]
+where
+    W: Copy,
+{
+    /// # Panics
+    ///
+    /// Panics if `s` is out of bounds.
+    ///
+    /// # Complexity
+    ///
+    /// O(1)
+    fn iter_weighted_edges(&self, s: usize) -> impl Iterator<Item = (usize, W)> {
+        self[s].iter().copied()
+    }
+}
+
+impl<const V: usize, W> IterWeightedEdges<W> for [HashSet<(usize, W)>; V]
 where
     W: Copy,
 {
