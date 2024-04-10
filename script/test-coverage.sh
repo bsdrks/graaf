4d933b0ce006605f3c01f15623b534c52a266a77
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-04-10 21:12:40 +0200

    chore: improve testing script to only run `lib` tests and also generate `lcov.info`

--- script/test-coverage.sh
+++ script/test-coverage.sh
@@ -1,4 +1,5 @@
-rm -rf ./target
+rm -rf ./target/coverage
 rm -rf ./coverage
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='coverage/cargo-test-%p-%m.profraw' cargo test
+CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='coverage/cargo-test-%p-%m.profraw' cargo test --lib
 grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/
+grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o coverage/lcov.info
