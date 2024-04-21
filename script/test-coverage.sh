d3705efcc3ee41da3f0cdf60299b5a31768c9ccf
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-04-21 10:03:17 +0200

    chore: remove `--branch` flag from `grcov` commands

--- script/test-coverage.sh
+++ script/test-coverage.sh
@@ -1,5 +1,5 @@
 rm -rf ./target/debug
 rm -rf ./coverage
 CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='coverage/cargo-test-%p-%m.profraw' cargo test --lib
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/
-grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o coverage/lcov.info
+grcov . --binary-path ./target/debug/deps/ -s . -t html --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/
+grcov . --binary-path ./target/debug/deps/ -s . -t lcov --ignore-not-existing --ignore '../*' --ignore "/*" -o coverage/lcov.info
